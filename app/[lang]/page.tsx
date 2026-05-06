7fa791e61e838f6f9000e4a5ea18d732a103f736
yaakupkara <yakup.kara@ibss.com.tr>
2026-05-06 22:59:40 +0300

    fix: lang prop added to sections

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,11 +1,17 @@
 import { HeroSection } from "@/components/sections/home/HeroSection";
 import { PhilosophySection } from "@/components/sections/home/PhilosophySection";
 
-export default function Home() {
+export default async function Home({
+  params,
+}: {
+  params: Promise<{ lang: string }>;
+}) {
+  const { lang } = await params;
+
   return (
     <div>
-      <HeroSection />
-      <PhilosophySection />
+      <HeroSection lang={lang as "en" | "tr"} />
+      <PhilosophySection lang={lang as "en" | "tr"} />
     </div>
   );
 }
